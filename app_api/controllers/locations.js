b49722a54fee3c2c7edb329d800a1a25dceee95b
Angel Jimenez <ajl890889702@csu.fullerton.edu>
2016-08-05 14:17:54 -0700

    Express now talks to api

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -23,16 +23,31 @@ var sendJsonResponse = function (res, status, content) {
 	res.json(content);
 }
 
-/*
-locationsListByDistance
-locationsCreate
-ctrlLocations.locationsReadOne
-ctrlLocations.locationsUpdateOne
-ctrlLocations.locationDeleteOne
-*/
 
 module.exports.locationsCreate = function (req, res){
-	sendJsonResponse(res, 200, {"status": "success"});
+	Loc.create({
+		name: req.body.name,
+		address: req.body.address,
+		facilities: req.body.facilities.split(","),
+		coords:[parseFloat(req.body.lng), parseFloat(req.body.lat)],
+		openingTimes: [{
+			days: req.body.days1,
+			opening: req.body.opening1,
+			closing: req.body.closing1,
+			closed: req.body.closed1
+		}, {
+			days: req.body.days2,
+			opening: req.body.opening2,
+			closing: req.body.closing2,
+			closed: req.body.closed2
+		}]
+	}, function(err, location) {
+		if (err) {
+			sendJsonResponse(res, 400, err);
+		} else {
+			sendJsonResponse(res, 201, location);
+		}
+	});
 };
 
 module.exports.locationsListByDistance = function (req, res){
@@ -45,7 +60,7 @@ module.exports.locationsListByDistance = function (req, res){
 
 	var geoOptions = {
 		spherical : true,
-		maxDistance : theEarth.getRadsFromDistance(20);
+		maxDistance : theEarth.getRadsFromDistance(20),
 		num : 10
 	};
 
@@ -100,9 +115,69 @@ module.exports.locationsReadOne= function (req, res){
 };
 
 module.exports.locationsUpdateOne = function (req, res){
-	sendJsonResponse(res, 200, {"status": "success"});
+	if(!req.params.locationid) {
+		sendJsonResponse(res, 404, {
+			"message" : "Not found, locationid is required"
+		});
+		return;
+	}
+	Loc
+	.findById(req.params.locationid)
+	.select('-reviews -rating')
+	.exec(
+		function(err, location) {
+			if(!location) {
+				sendJsonResponse(res, 404, {
+					"message": "locationid not found"
+				});
+				return;
+			} else if (err) {
+				sendJsonResponse(res, 400, err);
+				return;
+			}
+			location.name = req.body.name;
+			location.address = req.body.address;
+			location.facilities = req.body.facilities.split("");
+			location.coords = [parseFloat(req.body.lng), parseFloat(req.body.lat)];
+			location.openingTimes = [{
+				days: req.body.days1,
+				opening: req.body.opening1,
+				closing: req.body.closing1,
+				closed: req.body.closed1
+			}, {
+				days: req.body.days2,
+				opening: req.body.opening2,
+				closing: req.body.closing2,
+				closed: req.body.closed2
+			}];
+			location.save(function(err, location) {
+				if (err) {
+					sendJsonResponse(res, 404, err);
+				} else {
+					sendJsonResponse(res, 200, location);
+				}
+			});
+		}
+	);
 };
 
 module.exports.locationsDeleteOne = function (req, res){
-	sendJsonResponse(res, 200, {"status" : "success"});
+	var locaionid = req.params.locationid;
+	if (locationid) { 
+		Loc
+			.findByIdAndRemove(locationid)
+			.exec(
+			function(err, location) {
+				if (err) {
+					sendJsonResponse(res, 404, err) ;
+					return; 
+				}
+				sendJsonResponse(res, 204, null) ;
+			}
+		);
+	} else {
+		sendJsonResponse(res, 404, {
+			"message": "No locationid"
+		});
+	}
 };
